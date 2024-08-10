76263ac409ef0e2dc9a720c7ae774b4dc0321779
Sark <uitrinken1@gmail.com>
2024-08-10 15:15:26 +0700

    one big commit for all

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import GameHeader from './components/GameHeader.vue'
+import GameFigure from './components/GameFigure.vue'
+import GameWrongLetters from './components/GameWrongLetters.vue'
+import GameWord from './components/GameWord.vue'
+import GamePopup from './components/GamePopup.vue'
+import GameNotification from './components/GameNotification.vue'
+import { ref, watch } from 'vue'
+import { useRandomWord } from './composables/useRandomWord'
+import { useLetters } from './composables/useLetters'
+import { useNotification } from './composables/useNotofocation'
+
+const { word, getRandomWord } = useRandomWord()
+const { letters, correctLetters, wrongLetters, isLose, isWin, addLetter, resetLetters } =
+  useLetters(word)
+const { notification, showNotification } = useNotification()
+const popup = ref<InstanceType<typeof GamePopup> | null>(null)
+const restart = async () => {
+  await getRandomWord()
+  resetLetters()
+  popup.value?.close()
+}
+
+watch(wrongLetters, () => {
+  if (isLose.value) {
+    popup.value?.open('lose')
+  }
+})
+
+watch(wrongLetters, () => {
+  if (isWin.value) {
+    popup.value?.open('win')
+  }
+})
+
+window.addEventListener('keydown', ({ key }) => {
+  if (isLose.value || isWin.value) {
+    return
+  }
+
+  if (letters.value.includes(key)) {
+    showNotification()
+    return
+  }
+
+  addLetter(key)
+})
+</script>
+
+<template>
+  <GameHeader />
+  <div class="game-container">
+    <GameFigure :wrong-letters-count="wrongLetters.length" />
+    <GameWrongLetters :wrong-letters="wrongLetters" />
+    <GameWord :word="word" :correctLetters="correctLetters" />
+  </div>
+
+  <GamePopup :word="word" @click="restart()" ref="popup" />
+  <GameNotification ref="notification" />
+</template>
+
+<style lang="scss" scoped></style>
